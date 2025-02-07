88c39164bb5ab9ae9cb434eba2b6b812609747dc
mahmuud313 <111141511+mahmuud313@users.noreply.github.com>
2025-02-07 18:59:48 +0200

    Update script-1.js

--- script-1.js
+++ script-1.js
@@ -1,14 +1,19 @@
-window.onload = function () {
-    const message = document.getElementById('message');
-    message.style.opacity = 0;
-    let fadeEffect = setInterval(function () {
-        if (!message.style.opacity) {
-            message.style.opacity = 1;
-        }
-        if (message.style.opacity < 1) {
-            message.style.opacity = parseFloat(message.style.opacity) + 0.1;
-        } else {
-            clearInterval(fadeEffect);
-        }
-    }, 100);
-};
+function checkPassword() {
+    var password = document.getElementById("password-input").value;
+    if (password === "22022025") {  // Change this password
+        document.getElementById("login-page").style.display = "none";
+        document.getElementById("album-page").classList.remove("hidden");
+    } else {
+        alert("Wrong password! Try again.");
+    }
+}
+
+function showMessagePage() {
+    document.getElementById("album-page").classList.add("hidden");
+    document.getElementById("message-page").classList.remove("hidden");
+}
+
+function showAlbumPage() {
+    document.getElementById("message-page").classList.add("hidden");
+    document.getElementById("album-page").classList.remove("hidden");
+}
